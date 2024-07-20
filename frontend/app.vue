ce36bc12598e8fb77d6c9a268e9b5a3562cb2873
Alan Balen Schio <alanbschio@gmail.com>
2024-07-20 00:34:01 -0400

    chore: adding fe and be samples

---
+++ frontend/app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const router = useRouter()
+useHead({
+  bodyAttrs: {
+    class: 'h-full bg-white'
+  }
+})
+</script>
+
+<template>
+  <!-- <NuxtExample dir="routing/pages" :nav="nav" current-route>
+    <NuxtLoadingIndicator /> -->
+  <main>
+    <NuxtPage />
+  </main>
+  <!-- </NuxtExample> -->
+</template>
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  extends: [
+    '@nuxt/examples-ui'
+  ],
+  modules: ["@nuxt/icon"]
+})
