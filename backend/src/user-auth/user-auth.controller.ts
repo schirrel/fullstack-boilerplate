f733e84ba9e807d5aa727a1b4ab3f2078f5ebfb2
Alan Balen Schio <alanbschio@gmail.com>
2024-07-20 00:37:01 -0400

    Adding missing files for auth

--- backend/src/user-auth/user-auth.controller.ts
+++ backend/src/user-auth/user-auth.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { UserAuthService } from './user-auth.service';
+import { User } from './schemas/user-auth.schema';
+import { AuthGuard } from './auth.guard';
 
-@Controller('user-auth')
-export class UserAuthController {}
+@Controller('api/auth')
+export class UserAuthController {
+  constructor(private readonly userAuthService: UserAuthService) {}
+
+  @Post('register')
+  async registerUser(@Body() body: { username: string; password: string }): Promise<{ message: string }> {
+    const { username, password } = body;
+    await this.userAuthService.registerUser(username, password);
+    return { message: 'User registered successfully' };
+  }
+
+  @Post('login')
+  async loginUser(@Body() body: { username: string; password: string }): Promise<{ message: string; token: string }> {
+    const { username, password } = body;
+    const token = await this.userAuthService.loginUser(username, password);
+    return { message: 'Login successful', token };
+  }
+
+  @Get('users')
+  @UseGuards(AuthGuard)
+  async getUsers(): Promise<User[]> {
+    return this.userAuthService.getUsers();
+  }
+}
--- backend/src/user-auth/user-auth.module.ts
+++ backend/src/user-auth/user-auth.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common';
-import { UserAuthService } from './user-auth.service';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UserAuthController } from './user-auth.controller';
+import { UserAuthService } from './user-auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/user-auth.schema';
+import { secretKey } from './config'; 
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    JwtModule.register({
+      secret: secretKey.secret,
+      signOptions: { expiresIn: '1h' }, 
+    }),
+  ],
+  controllers: [UserAuthController],
   providers: [UserAuthService],
-  controllers: [UserAuthController]
 })
-export class UserAuthModule {}
+
+export class UserAuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+   }
+}
